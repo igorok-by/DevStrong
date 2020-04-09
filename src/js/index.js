f77c24990fb555cf88c60f43365d875e753a1e0e
Igor <igorko.by@gmail.com>
2020-04-09 23:39:04 +0300

    feat(sections): add carousels Add sections: Bestsellers, Latest, News, About

--- src/js/index.js
+++ src/js/index.js
@@ -10,6 +10,39 @@ $(function () {
     autoplay: false,
     autoplaySpeed: 7000,
   });
+
+  $('#carousel-1').slick({
+    arrows: true,
+    appendArrows: $('.carousel-arrows--best'),
+    nextArrow: $('#arrNext-1'),
+    prevArrow: $('#arrPrev-1'),
+    dots: false,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: false,
+  });
+
+  $('#carousel-2').slick({
+    arrows: true,
+    appendArrows: $('.carousel-arrows--latest'),
+    nextArrow: $('#arrNext-2'),
+    prevArrow: $('#arrPrev-2'),
+    dots: false,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: false,
+  });
+
+  $('#carousel-3').slick({
+    arrows: true,
+    appendArrows: $('.carousel-arrows--news'),
+    nextArrow: $('#arrNext-3'),
+    prevArrow: $('#arrPrev-3'),
+    dots: false,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: false,
+  });
   
   // $('.carousel__wrap-2').slick({
   //   arrows: false,
@@ -27,8 +60,8 @@ $(function () {
   // });
   
   // $('.carousel__wrap-3').slick({
-  //   arrows: true,
-  //   appendArrows: $('.carousel__arrows'),
+    // arrows: true,
+    // appendArrows: $('.carousel__arrows'),
   //   nextArrow: $('#arrNext-1'),
   //   prevArrow: $('#arrPrev-1'),
   //   asNavFor: '.carousel__wrap-2',
