c693b3f48d882b28627c722608839e00e6f02357
Igor <igorko.by@gmail.com>
2020-04-10 11:16:54 +0300

    style: prettify style by css-comb

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,3 @@
-// Main js file
 
 $(function () {
 
@@ -7,7 +6,7 @@ $(function () {
     dots: true,
     dotsClass: 'carousel__dots',
     arrows: false,
-    autoplay: false,
+    autoplay: true,
     autoplaySpeed: 7000,
   });
 
@@ -43,37 +42,4 @@ $(function () {
     slidesToScroll: 1,
     autoplay: false,
   });
-  
-  // $('.carousel__wrap-2').slick({
-  //   arrows: false,
-  //   asNavFor: '.carousel__wrap-3',
-  //   dots: true,
-  //   dotsClass: 'carousel__dots',
-
-  //   responsive: [{
-  //     breakpoint: 576,
-  //     settings: {
-  //       autoplay: true,
-  //       autoplaySpeed: 7000
-  //     }
-  //   }]
-  // });
-  
-  // $('.carousel__wrap-3').slick({
-    // arrows: true,
-    // appendArrows: $('.carousel__arrows'),
-  //   nextArrow: $('#arrNext-1'),
-  //   prevArrow: $('#arrPrev-1'),
-  //   asNavFor: '.carousel__wrap-2',
-  //   cssEase: 'linear',
-  //   speed: 500,
-  //   fade: true,
-
-  //   responsive: [{
-  //     breakpoint: 575.98,
-  //     settings: {
-  //       arrows: false
-  //     }
-  //   }]
-  // })
 });
